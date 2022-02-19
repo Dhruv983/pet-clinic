b3043995897351c9166c08b6b99bb51f51ff19e6
dhruv <dhruv.pualsa@gmail.com>
2022-02-19 16:09:29 +0530

    Lecture 122 done

---
+++ pet-clinic-data/src/main/java/com/demo/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.demo.petclinic.services;
+
+import com.demo.petclinic.model.PetType;
+
+public interface PetTypeService extends CRUDService<PetType,Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/demo/petclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,35 @@
+package com.demo.petclinic.services.map;
+
+import com.demo.petclinic.model.PetType;
+import com.demo.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType,Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findall();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
