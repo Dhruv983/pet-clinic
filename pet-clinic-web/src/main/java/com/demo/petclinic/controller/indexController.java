a074118e0d9a4c1b85dcfefdb3082aa490558dbd
dhruv <dhruv.pualsa@gmail.com>
2022-02-19 19:04:32 +0530

    Lecture 128 done

--- pet-clinic-web/src/main/java/com/demo/petclinic/controller/indexController.java
+++ pet-clinic-web/src/main/java/com/demo/petclinic/controller/indexController.java
@@ -1,12 +1,21 @@
 package com.demo.petclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class indexController {
+
     @RequestMapping({"","/","index","index.html"})
     public String index(){
         return "index";
     }
+
+    @RequestMapping({"/oups"})
+    public String oupsHandler(Model model){
+        return "notimplemented";
+    }
+
+
 }
