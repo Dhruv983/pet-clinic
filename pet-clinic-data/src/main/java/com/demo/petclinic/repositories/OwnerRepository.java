0bca816ca0288288216d202769362060d0768c18
dhruv <dhruv.pualsa@gmail.com>
2022-02-20 15:37:32 +0530

    Lecture 150 done

---
+++ pet-clinic-data/src/main/java/com/demo/petclinic/repositories/OwnerRepository.java
@@ -0,0 +1,8 @@
+package com.demo.petclinic.repositories;
+
+import com.demo.petclinic.model.Owner;
+import com.demo.petclinic.services.CRUDService;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OwnerRepository extends CrudRepository<Owner,Long> {
+}
