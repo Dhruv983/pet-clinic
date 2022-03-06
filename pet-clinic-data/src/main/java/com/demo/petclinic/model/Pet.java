f3e3e58302409486312d81192b7d7001a3b18795
dhruv <dhruv.pualsa@gmail.com>
2022-03-06 13:51:30 +0530

    Lecture 221 done

--- pet-clinic-data/src/main/java/com/demo/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/demo/petclinic/model/Pet.java
@@ -10,8 +10,19 @@ import java.util.Set;
 @Setter
 @Getter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "pets")
 public class Pet extends BaseEntity{
+    @Builder
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
+        super(id);
+        this.name = name;
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+        this.visits = visits;
+    }
 
     @Column(name = "name")
     private String name;
